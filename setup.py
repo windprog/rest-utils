ad2cd14430ed1110498a85296a1c13b02abf5c99
Windpro <windprog@gmail.com>
2017-11-16 15:44:52 +0800

    change version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ Desc    :
 """
 from setuptools import setup
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "windprog"
 __author_email__ = "windprog@gmail.com"
 __description__ = "gunicorn wrapper. support worker"
