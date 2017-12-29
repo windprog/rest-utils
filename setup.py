355ac595ba527440cc73f6ee9ae1912560d0e149
Windpro <windprog@gmail.com>
2017-12-29 16:35:21 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ Desc    :
 """
 from setuptools import setup
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "windprog"
 __author_email__ = "windprog@gmail.com"
 __description__ = "gunicorn wrapper. support worker"
