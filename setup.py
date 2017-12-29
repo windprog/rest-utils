005298d2b72a3ee59b1a2cb1f7b5fac1fe163f8d
Windpro <windprog@gmail.com>
2017-12-29 17:08:38 +0800

    fix bug

--- setup.py
+++ setup.py
@@ -9,19 +9,21 @@ E-mail  :   windprog@gmail.com
 Date    :   14-12-1
 Desc    :   
 """
+import os
+from pip.req import parse_requirements
+from pip.download import PipSession
 from setuptools import setup
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 __author__ = "windprog"
 __author_email__ = "windprog@gmail.com"
 __description__ = "gunicorn wrapper. support worker"
 __title__ = 'rest-utils'
 __url__ = "https://github.com/windprog/rest-utils"
 
-try:
-    INSTALL_REQUIRES = [r for r in open('requirements.txt').read().split('\n') if len(r) > 0]
-except:
-    INSTALL_REQUIRES = []
+req = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+install_reqs = parse_requirements(req, session=PipSession())
+install_requires = [str(ir.req) for ir in install_reqs]
 
 setup(
     name=__title__,
@@ -31,5 +33,6 @@ setup(
     author=__author__,
     author_email=__author_email__,
     packages=['rest_utils', 'rest_utils.worker'],
-    install_requires=INSTALL_REQUIRES
+    install_requires=install_requires,
+    data_files=[(".", ['requirements.txt'])]
 )
