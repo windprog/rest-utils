a667f3922985577a1df4289166d95146a38bc203
Windpro <windprog@gmail.com>
2019-02-23 15:18:12 +0800

    change docs

--- example/auto_create_admin.py
+++ example/auto_create_admin.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # wget https://github.com/lerocha/chinook-database/raw/master/ChinookDatabase/DataSources/Chinook_Sqlite_AutoIncrementPKs.sqlite
+# pip install flask_admin enum34
 import flask
 from flask_sqlalchemy import SQLAlchemy
 from rest_utils import APIManager
 
 app = flask.Flask(__name__)
 app.secret_key = "test"
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../tests/data/Chinook_Sqlite_AutoIncrementPKs.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./Chinook_Sqlite_AutoIncrementPKs.sqlite'
 db = SQLAlchemy(app)
 api = APIManager(app, db=db)
 api.auto_create()
 api.register_admin()
-app.run()
+app.run(host="0.0.0.0")
 # curl http://127.0.0.1:5000/api/Track?_num=20&_expand=1
 # open http://127.0.0.1:5000/admin
